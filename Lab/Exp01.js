e9df00a825215bac15d8d2b34dff03b2b7525505
Satyajeet Patil <sattupatil5527@gmail.com>
2026-01-22 19:52:34 +0530

    WT

--- Lab/Exp01.js
+++ Lab/Exp01.js
@@ -1,72 +1,82 @@
-//activtiy-1
-let fName="Yash"
-let lName="Patil"
-let age= 20
-let dept="Ai/ML"
-let div ="b"
-let rollNo =109
-let grade= "A+"
-let pass= true
+// activity-1
+let fName = "Yash";
+let lName = "Patil";
+let age = 20;
+let dept = "Ai/ML";
+let div = "b";
+let rollNo = 109;
+let grade = "A+";
+let pass = true;
 
-console.log("First name :",fName);
-document.writeln(fName)
-console.log("Last name  :",lName);
-document.writeln(lName)
-console.log("Age        :",age);
-document.writeln(age)
-console.log("Department :",dept);
-document.writeln(dept)
-console.log("Division   :",div);
-document.writeln(div)
-console.log("Roll no.   :",rollNo);
-document.writeln(rollNo)
-console.log("Grade      :",grade,);
-document.writeln(grade)
-console.log("Is passed? :",pass);
-document.writeln(pass)
+console.log("First name :", fName);
+document.writeln(fName + "<br>");
 
-//activity-2
-let num1=20;
-if(num1%2==0){
-    console.log("Even number.")
-    document.writeln(num1)
+console.log("Last name :", lName);
+document.writeln(lName + "<br>");
+
+console.log("Age :", age);
+document.writeln(age + "<br>");
+
+console.log("Department :", dept);
+document.writeln(dept + "<br>");
+
+console.log("Division :", div);
+document.writeln(div + "<br>");
+
+console.log("Roll no :", rollNo);
+document.writeln(rollNo + "<br>");
+
+console.log("Grade :", grade);
+document.writeln(grade + "<br>");
+
+console.log("Is passed? :", pass);
+document.writeln(pass + "<br><br>");
+
+
+// activity-2
+let num1 = 20;
+
+if (num1 % 2 === 0) {
+    console.log("Even number.");
+    document.writeln(num1 + "<br><br>");
+} else {
+    console.log("Odd number.");
+    document.writeln(num1 + "<br><br>");
 }
-else{
-    console.log("Odd number.")
-    document.writeln(num1)
 
-}
 
-//activity-3
-let marks= 10;
-if(marks>=40){
+// activity-3
+let marks = 10;
+
+if (marks >= 40) {
     console.log("Pass.");
-    document.writeln(marks)
-
-}
-else{
-    console.log("Fail.")
-    document.writeln(marks)
-
+    document.writeln(marks + "<br><br>");
+} else {
+    console.log("Fail.");
+    document.writeln(marks + "<br><br>");
 }
 
-//activity-4
-let num2=10;
-let i;
-for(i=1;i<=10;i++){
-    console.log(i)
-    document.writeln(num2)
 
+// activity-4
+let num2 = 10;
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+    document.writeln(i + "<br>");
 }
 
-//activity-5
-let x=10
-let y=x
+document.writeln("<br>");
+
+
+// activity-5
+let x = 10;
+let y = x;
+
 {
-    let y=20
-    console.log(x,y);
-    document.writeln(x,y)
-
+    let y = 20;
+    console.log(x, y);
+    document.writeln(x + " " + y + "<br>");
 }
-console.log(x,y);
-document.writeln(x,y)
+
+console.log(x, y);
+document.writeln(x + " " + y);
